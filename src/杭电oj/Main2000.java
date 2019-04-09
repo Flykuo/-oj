b25e7aae40728d2d6e3d5cd0b27a1a6561dbf180
25136 <25136@Flykuo>
2019-04-09 20:40:59 +0800

    2000 成功ac

--- src/杭电oj/Main2000.java
+++ src/杭电oj/Main2000.java
@@ -1,10 +1,25 @@
 package º¼µçoj;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main2000 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner (System.in);
+		while(sc.hasNext()) {
+			String str=sc.next();
+			char a[]=str.toCharArray();
+			Arrays.sort(a);
+			for(int i=0;i<a.length;i++) {
+				if(i==a.length-1) {
+					System.out.println(a[i]);
+				}else {
+					System.out.print(a[i]+" ");
+				}
+			}
+		}
 	}
 
 }
