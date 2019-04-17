7317f819462ea1888a15049605789eaf3eace498
25136 <25136@Flykuo>
2019-04-17 09:09:22 +0800

    查找每个字母出现的次数

--- src/杭电oj/Main1219.java
+++ src/杭电oj/Main1219.java
@@ -7,11 +7,7 @@ public class Main1219 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner (System.in);
-		int a=0;
 		while(sc.hasNext()) {
-			if(a!=0) {
-				System.out.println();
-			}
 			String str=sc.nextLine();
 			int arr[]=new int[26];
 			for(int i=0;i<str.length();i++) {
@@ -22,7 +18,7 @@ public class Main1219 {
 			for(int i=0;i<26;i++) {
 				System.out.println((char)(i+97)+":"+arr[i]);
 			}
-			a++;
+			System.out.println();
 		}
 	}
 
