a5de835780b2de20717e6e3a798bf9d26adbdec7
25136 <25136@Flykuo>
2019-08-14 12:07:18 +0800

    邻接表的匈牙利算法

--- src/杭电oj/Main2063.java
+++ src/杭电oj/Main2063.java
@@ -1,52 +1,69 @@
-package 杭电oj;
+package 鏉數oj;
 
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main2063 {
+    static final int max = 2001;
+    static int cnt, head[], lk[];
+    static Edge edge[];
+    static boolean vis[];
 
-	public static int max = 501, a[] = new int[max], b[] = new int[max], arr[][] = new int[max][max], n, m;
+    static void add_edge(int x, int y) {
+        edge[cnt].to = y;
+        edge[cnt].next = head[x];
+        head[x] = cnt++;
+    }
 
-	public static boolean find(int x) {
-		for (int i = 1; i <= n; i++) {
-			if (arr[x][i] == 1 && b[i] == 0) {// 有关系且男生没有被选
-				b[i] = 1;// 标记已经被选
-				if (a[i] == 0 || find(a[i])) {// 没有选择女生 或者可以更改女生
-					a[i] = x;
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    static boolean find(int x) {
+        for (int i = head[x]; i != -1; i = edge[i].next) {
+            if (vis[edge[i].to]) {
+                continue;
+            }
+            vis[edge[i].to] = true;
+            if (lk[edge[i].to] == -1 || find(lk[edge[i].to])) {
+                lk[edge[i].to] = x;
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			int k = sc.nextInt();
-			if (k == 0) {
-				return;
-			}
-			m = sc.nextInt();
-			n = sc.nextInt();
-			for(int i=0;i<501;i++) {
-				Arrays.fill(arr[i],0);
-			}
-			Arrays.fill(a, 0);
-			Arrays.fill(b, 0);
-			for (int i = 0; i < k; i++) {
-				int x = sc.nextInt();
-				int y = sc.nextInt();
-				arr[x][y] = 1;
-			}
-			int sum = 0;
-			for (int i = 1; i <= m; i++) {
-				Arrays.fill(b, 0);
-				if (find(i)) {
-					sum++;
-				}
-			}
-			System.out.println(sum);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int k = sc.nextInt();
+            if (k == 0) {
+                return;
+            }
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            head = new int[m + 1];
+            vis = new boolean[n+1];
+            Arrays.fill(head, -1);
+            lk = new int[n + 1];
+            Arrays.fill(lk, -1);
+            edge = new Edge[k+1];
+            for (int i = 0; i <=k; i++) {
+                edge[i] = new Edge();
+            }
+            cnt = 1;
+            for (int i = 0; i < k; i++) {
+                add_edge(sc.nextInt(), sc.nextInt());
+            }
+            int ans = 0;
+            for (int i = 1; i <= m; i++) {
+                Arrays.fill(vis,false);
+                if (find(i)) {
+                    ans++;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+
+    static class Edge {
+        int to, next;
+    }
 }
